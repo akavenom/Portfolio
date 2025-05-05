8f19f8e4cfb26b4b71e899913186477ab31d788a
akavenom <112108332+akavenom@users.noreply.github.com>
2025-05-05 12:24:56 +0530

    script.js

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ document.querySelectorAll('.social-links a').forEach(link => {
         alert(`You clicked on ${event.target.textContent}`);
     });
 });
+
+    const menuToggle = document.getElementById('mobile-menu');
+    const navLinks = document.getElementById('nav-links');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
